952ec1ad0fbdcd70dde95f45540bde8d52493b32
John Nolan <jnolan5@nd.edu>
2017-11-11 15:39:28 -0500

    testing for german ngram model, much too high

--- witten_test.py
+++ witten_test.py
@@ -7,13 +7,13 @@ if __name__ == "__main__":
     #e_m = wittengram.Wittengram()
     #e_m.train("langs/english")
     #e_m.start()
-    print "1"
+    print("1")
     g_m = wittengram.Wittengram()
-    print "2"
-    g_m.train("langs/german")
-    print "3"
+    print("2")
+    g_m.train("German")
+    print("3")
     g_m.start()
-    print "4"
+    print("4")
 
     #i_m = wittengram.Wittengram()
     #i_m.train("langs/irish")
@@ -29,7 +29,16 @@ if __name__ == "__main__":
 
 
     for line in open("langs/finnegans.txt"):
-        for w in line.rstrip('\n'):
-            print g_m.prob(w) #gets the likelihood of following current set of characters
+        max_p = 0.0
+        best = 0
+        for i, w in enumerate(line.rstrip('\n')):
+            prob = g_m.prob(w) #probability of this character in german
+            if prob > max_p:
+                max_p = prob
+                best = i
             g_m.read(w)
+        l = best - 5
+        if best - 5 < 0:
+            l = 0
+        print(line.rstrip('\n')[l:best + 1], max_p)
             #incorporate new data to model (necessary to keep track of grams)
