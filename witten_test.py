d5d3fe86a4dbbf297cfe692159ff8f825b162e23
John Nolan <jnolan5@nd.edu>
2017-11-11 14:10:47 -0500

    Modified witten_test for our testing purposes

--- witten_test.py
+++ witten_test.py
@@ -8,17 +8,8 @@ if __name__ == "__main__":
 	m.train("english/train")
 	m.start()
 	
-	correct = 0
-	total = 0
-	
 	for line in open("english/dev"):
 		for w in line.rstrip('\n'): 
-			if m.predict() == w: #predict will return the most likely character in context
-			# need likelihood of character rather than prediction
-				correct += 1
-			total += 1
+			m.prob(w) #gets the likelihood of following current set of characters
 			m.read(w) 
 			#incorporate new data to model (necessary to keep track of grams)
-
-	success = 100.0*float(correct/total)
-	print("%{0} success".format(success)) #print success rate
