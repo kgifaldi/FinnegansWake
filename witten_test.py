c91373ed612d3f557890cbeac590266116070284
Kyle <kgifaldi@nd.edu>
2017-11-11 14:34:04 -0500

    added langs folder

--- witten_test.py
+++ witten_test.py
@@ -2,14 +2,34 @@ import wittengram
 
 if __name__ == "__main__":
 
-	"""Program to train and test the fivegram model"""
+    """Program to train and test the fivegram model"""
 
-	m = wittengram.Wittengram()
-	m.train("english/train")
-	m.start()
-	
-	for line in open("english/dev"):
-		for w in line.rstrip('\n'): 
-			m.prob(w) #gets the likelihood of following current set of characters
-			m.read(w) 
-			#incorporate new data to model (necessary to keep track of grams)
+    #e_m = wittengram.Wittengram()
+    #e_m.train("langs/english")
+    #e_m.start()
+    print "1"
+    g_m = wittengram.Wittengram()
+    print "2"
+    g_m.train("langs/german")
+    print "3"
+    g_m.start()
+    print "4"
+
+    #i_m = wittengram.Wittengram()
+    #i_m.train("langs/irish")
+    #i_m.start()
+
+    #f_m = wittengram.Wittengram()
+    #f_m.train("langs/french")
+    #f_m.start()
+
+    #d_m = wittengram.Wittengram()
+    #d_m.train("langs/danish")
+    #d_m.start()
+
+
+    for line in open("langs/finnegans.txt"):
+        for w in line.rstrip('\n'):
+            print g_m.prob(w) #gets the likelihood of following current set of characters
+            g_m.read(w)
+            #incorporate new data to model (necessary to keep track of grams)
