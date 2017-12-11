3f2c0baa8211125b973370c26a512945fe3b67c4
John Nolan <jnolan5@nd.edu>
2017-12-11 16:26:55 -0500

    prepared to make data with taggins

--- finnegans_MLP.py
+++ finnegans_MLP.py
@@ -9,78 +9,139 @@ from keras.optimizers import SGD
 from keras.preprocessing.text import hashing_trick, Tokenizer
 from nltk.tag import StanfordPOSTagger
 from nltk import word_tokenize
+import numpy as np
+import os
 
-german_files = ["langs/german/pg27769.txt"]
+class FinnegansMLP:
 
-def main():
-	model = Sequential()
-	german = ""#"Ding an sich"#make_data()
-	for file_name in german_files:
-		with open(file_name) as gf:
-			for line in gf:
-				for c in line: 
-					if c.isalpha():
-						german += c + " " #forcing characterlevel hash
-	#need to find number of letter types, n, and tag types, nt, n*t will be our vector size
-	#we can cut out common letters, so I hope the result will be 50*20, close to 1000
-	n = 100#how many letter_tag types we expect to see
-	model.add(Embedding( n, 15, input_length=7))
-	#model.add(Dropout(0.5)) #TODO: look into Dropout
-	#model.add(Dense(7, activation='relu'))
-	#model.add(Dropout(0.5))
-	model.add(Dense(4, activation='softmax')) #4 classes
+	def __init__(self, n, i):
+		"""Define letter vocab size, n, and input size, i"""
 
-	#sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-	model.compile(loss='categorical_crossentropy',
-			optimizer='adam', #sgd, #'rmsprop' might also work, or 'adam'
-			metrics=['accuracy'])
+		self.n = n
+		self.isize = i
+		self.categories = ["french", "german", "irish", "joyce"]
 
-	#then our data will be a set of vectors with the n*t range described above,
-	#telling us the character and POS tag. (all special characters can be unk)
-	# the vectors will be length 7, so shape will be batch_size, 7. 
+	def compile_data(self, cat):
+		"""Compile data for the given category (indexed in the following list)"""
 
-	data = hashing_trick(german, n, hash_function='md5', lower=False, split=' ')
-	for c, i in zip(german.split(' '), data):
-		print(c, i) #just to show all characters are lined up with the hash
-	#data = Tokenizer(n, lower=False, char_level=True).texts_to_matrix(german)
-	#print(data)
-	#with this data, will cut it into groups of 7 or whatever (this is just a list)
-	# and also make a label for each of these groups with the german integer value
+		n = self.n
+		i = self.isize
 
-	#Then we will have labes with 4 possible values (Joyce, Irish, French, German) 
-	#An array of batchsize vectors, 1D with values ranging within 4. convert these:
-	#mix up labels in the same order as groups of characters
-	#one_hot_labels = keras.utils.to_categorical(labels, num_classes=4)
+		text = ""
+		for filename in os.listdir("./langs/" + self.categories[cat]):
+			with open(filename) as tf:
+				for line in tf:
+					for c in line:
+						if c.isalpha():
+							text += c + " " #setup for character level hashing
+						elif c == " ":
+							text += "_ "
 
-	#model.fit(data, one_hot_labels, epochs = 10, batch_size= ? )
-	#prediction = model.predict(finnegans_clips, batch_size = ?, verbose=1) # should give us batch_size vectors of classifications,
-	#so we print the parts it predicted as german. That's why it's key not to use RNN, or it might all be Joyce
-	#go back through finnegans and print the segments that were tagged as german (and line number can be stored
-	# with data, zip through it)
-	#or do predict_on_batch for eatch line part of finnegans wake.
+		raw_data = hashing_trick(text, n, hash_function='md5', lower=False, split=' ')
 
-	#another great idea: shuffle training samples together, should train much better
+		#with this data, will cut it into groups of isize (this is just a list)
+		# and also make a label for each of these groups with the category index 
+		labels = []
+		data = []
+		for i in range(0, len(raw_data), isize):
+			data.append(raw_data[i:i+isize])
+			labels.append(cat) #an index for each category is training target
+		
+		#make into numpy arrays
+		data = np.array(data)
+		labels = np.array(labels)
 
-def make_data():
-	#premake this stuff
-	jar = '../stanford-postagger-full-2017-06-09/stanford-postagger.jar'
-	model = '../stanford-postagger-full-2017-06-09/models/german-fast.tagger'
-	german_tagger = StanfordPOSTagger(model, jar)
-	german_with_tags = ""
-	#can make this isomorphic with dictionary of data lists
-	with open("german_data", "w+") as wf: #output all german data to a file
-		for i, file_name in enumerate(german_files):
-			with open(file_name) as gf:
-				for j, line in enumerate(gf):
-					text = german_tagger.tag(line.strip())
-					"""
+		return np.array(data), np.array(labels)
+
+	def run_model(self):
+		model = Sequential()
+		n = self.n#how many letter_tag types we expect to see
+		isize= self.isize
+		model.add(Embedding( n, 15, input_length=isize))
+		#model.add(Dropout(0.5)) #TODO: look into Dropout
+		#model.add(Dense(7, activation='relu'))
+		#model.add(Dropout(0.5))
+		model.add(Dense(4, activation='softmax')) #4 classes
+
+		#sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+		model.compile(loss='categorical_crossentropy',
+				optimizer='adam', #sgd, #'rmsprop' might also work, or 'adam'
+				metrics=['accuracy'])
+		data = np.array()
+		labels = np.array()
+		for i in range(4):
+			idata, ilabels = self.compile_data(i)
+			data.append(idata)
+			
+		one_hot_labels = keras.utils.to_categorical(labels, num_classes=4)
+
+		model.fit(data, one_hot_labels, epochs = 10, batch_size=32)
+
+		#prediction = model.predict(finnegans_clips, batch_size = 32, verbose=1) # should give us batch_size vectors of classifications,
+		#so we print the parts it predicted as german. That's why it's key not to use RNN, or it might all be Joyce
+		#go back through finnegans and print the segments that were tagged as german (and line number can be stored
+		# with data, zip through it)
+		#or do predict_on_batch for eatch line part of finnegans wake.
+
+		#another great idea: shuffle training samples together, should train much better
+
+	def make_data(self):
+		"""generate the tagged data (which takes ages)"""
+
+		jar = '../stanford-postagger-full-2017-06-09/stanford-postagger.jar'
+		german_model = '../stanford-postagger-full-2017-06-09/models/german-fast.tagger'
+		german_tagger = StanfordPOSTagger(german_model, jar)
+
+		with open("german_data", "w+") as wf: #output all german data to a file
+			for filename in os.listdir("./langs/german"):
+				with open("./langs/german/" + filename) as gf:
+					for line in gf:
+						text = german_tagger.tag(line.strip())
+						for word, tag in text: #, tag in text:
+							for c in word:
+								wf.write("_".join([c, tag]) + " ")
+							wf.write("_" + " ") #space character
+						if text:
+							wf.write("\n")
+
+		french_model = '../stanford-postagger-full-2017-06-09/models/french.tagger'
+		french_tagger = StanfordPOSTagger(french_model, jar)
+
+		with open("french_data", "w+") as wf: #output all french data to a file
+			for filename in os.listdir("./langs/french"):
+				with open("./langs/french/" + filename) as ff:
+					for line in ff:
+						text = french_tagger.tag(line.strip())
+						for word, tag in text: #, tag in text:
+							for c in word:
+								wf.write("_".join([c, tag]) + " ")
+							wf.write("_" + " ") #space character
+						if text:
+							wf.write("\n")
+		
+		english_model = '../stanford-postagger-full-2017-06-09/models/english-left3words-distsim.tagger'
+		english_tagger = StanfordPOSTagger(english_model, jar)
+		with open("finnegans_data", "w+") as wf:
+			with open("./langs/finnegans.txt") as fif:
+				for line in fif:
+					text = english_tagger.tag(line.strip())
 					for word, tag in text: #, tag in text:
 						for c in word:
 							wf.write("_".join([c, tag]) + " ")
-					"""
-					print(i, j)
-	return german_with_tags
+						wf.write("_" + " ") #space character
+					if text:
+						wf.write("\n")
+
+		#Then we will have labes with 4 possible values (Joyce, Irish, French, German) 
+		#An array of batchsize vectors, 1D with values ranging within 4. convert these:
+		#mix up labels in the same order as groups of characters
+
+		#our data could be a set of vectors with the n*t range described above,
+		#telling us the character and POS tag. (all special characters can be unk)
+		# the vectors will be length 'isize', so shape will be batch_size, isize. 
+		# see make_data for pos tagging
+
 
 if __name__=="__main__":
-	main()
-#	make_data() #this takes very long, tagging each item
+	fmodel = FinnegansMLP(100, 9) #vocab size and input length
+	fmodel.make_data() #this takes very long, tagging each item
--- witten_test.py
+++ witten_test.py
@@ -3,53 +3,58 @@ import os
 #from polyglot.detect import Detector
 
 
-def search_line(line, e_m, f_m, ofile):
+def search_line(line, e_m, f_m, ofile): #TODO: make sure we can change grams
     """search line for notable things to output"""
     best_p= [] #highest probability moments
+    last = 100
     for i, w in enumerate(line.rstrip('\n')):
         prob_j = e_m.prob(w) #probability that joyce was writing English
         prob = f_m.prob(w) #probability of this character in f
-        if prob > 0.90 and prob_j < 0.4:
+        if prob > 0.95 and prob_j < 0.5 and abs(last-i) > f_m.n/2: #want letters to be somewhat far apart
             best_p.append((i, prob))
+            last = i
         e_m.read(w)
         f_m.read(w)
     if best_p:
         ofile.write(line)
         for best, max_p in best_p:
-            l = best - 5 #get index of best match
-            if best - 5 < 0:
+            l = best - f_m.n #get index of best match
+            if best - f_m.n < 0:
                 l = 0  
-            ofile.write(line.rstrip('\n')[l:best + 1] + '\n')
+            if best-l > 2: #don't want short segments at beginning of sentences
+                ofile.write(line.rstrip('\n')[l:best + 1] + '\n')
         ofile.write('\n')
 
 def main():
 
     """Program to train and test the fivegram model"""
 
-    e_m = wittenmod.Wittengram()
+    n = 5
+
+    e_m = wittenmod.Wittengram(n)
     print("joyce")
     for tfile in os.listdir("./langs/joyce"):
         e_m.train("./langs/joyce/" + tfile)
         print(tfile + "...")
     e_m.start()
 
-    g_m = wittenmod.Wittengram()
+    g_m = wittenmod.Wittengram(n)
     print("German")
     for tfile in os.listdir("./langs/german"):
         g_m.train("./langs/german/" + tfile)
         print(tfile + "...")
     g_m.start()
 
-    i_m = wittenmod.Wittengram()
+    i_m = wittenmod.Wittengram(n)
     print("Irish")
     for tfile in os.listdir("./langs/irish"):
         i_m.train("./langs/irish/" + tfile)
         print(tfile + "...")
     i_m.start()
 
-    f_m = wittenmod.Wittengram()
+    f_m = wittenmod.Wittengram(n)
     print("French")
-    for tfile in os.listdir("./langs/french/"):
+    for tfile in os.listdir("./langs/french"):
         print(tfile + "...")
         f_m.train("./langs/french/" + tfile)
     f_m.start()
